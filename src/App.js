39158c7434d7ee17e1bbc2a658ee658492467eaf
Anjana Puri <anjana@i-motor.com.au>
2023-02-27 13:41:12 +1100

    added show counter button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
 
-
-const App = () => {
+const ToggleCounter = () => {
     const [count,setCount] = useState(0)
+    return (
+    <div>
+    <p>{count}</p>
+    <button onClick={() => setCount(count+1)} className="btn">more</button>
+    <button onClick={() => setCount(count-1)} className="btn">less</button>
+    <button onClick={() => setCount(0)}>reset</button>
+    </div>)
+}
+    
+const App = () => {
+    const [showCounter,setShowCounter] = useState(true)
+    let showHide = "show"
 return(
         <div>
-            <p>{count}</p>
-            <button onClick={() => setCount(count+1)} className="btn">more</button>
-            <button onClick={() => setCount(count-1)} className="btn">less</button>
-            <button onClick={() => setCount(0)}>reset</button>
+            <button onClick={() => setShowCounter(!showCounter)} className="btn">Show Counter</button>
+            {showCounter && <ToggleCounter/>}
         </div>
     )
+//todo - change button name 
 }
 
 export default App;
